ba00d74796e4b291c775cce9eb10c320099c4b94
CielLXE <xinerl2@illinois.edu>
2019-11-24 20:15:22 -0600

    add refactor

--- Liu_Xiner_ez_university/settings/production.py
+++ Liu_Xiner_ez_university/settings/production.py
@@ -2,4 +2,4 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'xiner.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'xinerl2.pythonanywhere.com']
